81433e3bb50befebb17337682141abe01ee144c4
satyam kumar <satyam.kumar@bnzgreen.io>
2024-10-14 02:05:03 +0530

    date.js

---
+++ date.js
@@ -0,0 +1,11 @@
+let now = new Date();
+console.log(now)
+
+// let specificDate1 = new Date("14-10-2024")
+// console.log(specificDate1)
+
+let specificDate2 = new Date("2024-10-13");
+console.log(specificDate2);
+
+let specificDate3 = new Date(2024, 10, 14, 10, 50, 30, 567)
+console.log(specificDate3)
