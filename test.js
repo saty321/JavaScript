00470984b6d00a2e2dc51c4f3239c3b31c6280e1
SATYAM KUMAR <ajsatyamaj123@gmail.com>
2024-10-13 07:45:55 +0000

    added test.js file

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 console.log("hello js")
+function isEvenOrOdd(number){
+    if(number%2) return `${number} is Even Number`
+    else return `${number} is Odd Number`
+}
+let result = isEvenOrOdd(845)
+console.log(result)
